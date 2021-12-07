9b7cae4f766e9bf77cb15ec79bc4521cc385e71b
Nantakud <francesco.vacca.au@gmail.com>
2021-12-07 13:06:01 +0800

    toggle arrow sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
 import "./sidebar.css";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { useState } from "react";
 
-export default function sidebar() {
+export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="sidebar">
-      <Link to="/">Home</Link>
-      <Link to="/projects">Projects</Link>
+    <div className="wrapper">
+      <i className="arrow" onClick={() => setIsOpen(!isOpen)}>
+        <FaChevronLeft />
+      </i>
+      {isOpen && (
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+        </div>
+      )}
     </div>
   );
 }
