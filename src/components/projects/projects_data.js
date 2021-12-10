541d9239707eec512e761fe2f8ae997744dfe6c3
Nantakud <francesco.vacca.au@gmail.com>
2021-12-10 11:30:52 +0800

    create project_data file

---
+++ src/components/projects/projects_data.js
@@ -0,0 +1,33 @@
+export const projects_data = [
+  {
+    title: "VideoGame",
+    description: "A revisitation of Space Invader",
+    video:
+      "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
+    stack: ["Node", "JS", "P5"],
+    source: "https://github.com/Nantakud/my_portfolio/commits/main",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "Rent a Movie",
+    description:
+      "You can rent a dvd online, even if nobody is doing it anymore",
+    video:
+      "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
+    stack: ["PHP", "JS"],
+    source: "https://github.com/Nantakud/my_portfolio/commits/main",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "Click and Collect",
+    description: "E-commerce solution for a fast food restaurant",
+    video:
+      "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
+    stack: ["Node", "JS", "P5"],
+    source: "https://github.com/Nantakud/my_portfolio/commits/main",
+    visit: "https://google.com",
+    id: 0,
+  },
+];
