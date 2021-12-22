e7a5218608523311f8a35e0dd49f681c0b205d4f
Nantakud <francesco.vacca.au@gmail.com>
2021-12-22 23:44:07 +0800

    added descriptions to project data

--- src/components/projects/projects_data.js
+++ src/components/projects/projects_data.js
@@ -1,132 +1,68 @@
 export const projects_data = [
   {
-    title: "VideoGame",
-    description: "A revisitation of Space Invader",
+    title: "Escape from Alcatraz",
+    description:
+      "Come out of your cell, dodge guards and dogs and run for your freedom",
     video:
       "https://www.youtube.com/embed/086aRavuRcQ?playlist=086aRavuRcQ&autoplay=1&mute=1&loop=1&controls=0",
-    stack: ["assets/nodejs.svg", "assets/javascript.svg", "assets/p5js.svg"],
-    source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: "https://google.com",
+    stack: ["assets/javascript.svg", "assets/p5js.svg"],
+    source: "https://github.com/Nantakud/escape_alcatraz",
+    visit: null,
     id: 0,
   },
   {
     title: "Rent a Movie",
     description:
-      "You can rent a dvd online, even if nobody is doing it anymore",
+      "A platform for a movies renting services. It provides several searching criteria, and, only for registered members, a wide selection of titles to rent. Admins have a dedicated section, with CRUD and report features.",
     video:
       "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
     stack: ["assets/php.svg", "assets/javascript.svg"],
     source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: null,
+    visit: "http://infotech.scu.edu.au/~fvacca10/movie_zone/",
     id: 1,
   },
   {
-    title: "Click and Collect",
-    description: "E-commerce solution for a fast food restaurant",
+    title: "Order Manager",
+    description:
+      "A wordpress plugin that allows to manage each step in a fast-food order cycle. It has dedicated interfaces for different team members and an handy click'n'collect button for the customer",
     video:
       "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
-    stack: ["assets/nodejs.svg", "assets/javascript.svg", "assets/p5js.svg"],
-    source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: "https://google.com",
+    stack: ["assets/php.svg", "assets/javascript.svg"],
+    source: "https://github.com/Nantakud/order_manager",
+    visit: "https://live-rooster-clickncollect.pantheonsite.io/",
     id: 2,
   },
   {
-    title: "Prova 1",
+    title: "React-quiz",
     description:
-      "Just adding more elements, so my test is more meaningful. ciao ciao carcifo",
+      "Test your knowledge! 10 always different questions, fetched from an external api",
     video:
       "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
-    stack: [
-      "assets/nodejs.svg",
-      "assets/angular.svg",
-      "assets/ionic.svg",
-      "assets/sass.svg",
-      "assets/react.svg",
-      "assets/nodejs.svg",
-      "assets/angular.svg",
-      "assets/ionic.svg",
-      "assets/phaser.svg",
-      "assets/react.svg",
-    ],
+    stack: ["assets/react.svg", "assets/sass.svg"],
     source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: "https://google.com",
+    visit: null,
     id: 3,
   },
   {
-    title: "Prova 2",
+    title: "Fix that Bug",
     description:
-      "Just adding more elements, so my test is more meaningful. ciao ciao pataa",
+      "Help a poor developer to catch all bugs before they run away and he gets fired!",
     video:
       "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
-    stack: ["assets/phaser.svg", "assets/react.svg"],
-    source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: "https://google.com",
+    stack: ["assets/javascript.svg", "assets/phaser.svg"],
+    source: "https://github.com/Nantakud/fix_that_bug_game",
+    visit: "https://francesco-vacca.itch.io/fix-that-bug",
     id: 4,
   },
   {
-    title: "Prova 3",
+    title: "Media rating",
     description:
-      "Just adding more elements, so my test is more meaningful. ciao ciao caroat",
+      "An android app, created as a platform for simple CRUD operation on a database",
     video:
       "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
-    stack: ["Node", "JS", "P5"],
+    stack: ["assets/angular.svg", "assets/ionic.svg"],
     source: "https://github.com/Nantakud/my_portfolio/commits/main",
     visit: "https://google.com",
     id: 5,
-  },
-  {
-    title: "Prova 4",
-    description:
-      "Just adding more elements, so my test is more meaningful. ciao ciao barba",
-    video:
-      "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
-    stack: ["Node", "JS", "P5"],
-    source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: "https://google.com",
-    id: 6,
-  },
-  {
-    title: "Prova 5",
-    description:
-      "Just adding more elements, so my test is more meaningful. ciao ciao clamaro",
-    video:
-      "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
-    stack: ["Node", "JS", "P5"],
-    source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: "https://google.com",
-    id: 7,
-  },
-  {
-    title: "Prova 6",
-    description:
-      "Just adding more elements, so my test is more meaningful. ciao ciao distase4",
-    video:
-      "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
-    stack: ["Node", "JS", "P5"],
-    source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: "https://google.com",
-    id: 8,
-  },
-  {
-    title: "Prova 7",
-    description:
-      "Just adding more elements, so my test is more meaningful. ciao ciao <ballosos></ballosos>",
-    video:
-      "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
-    stack: ["Node", "JS", "P5"],
-    source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: "https://google.com",
-    id: 9,
-  },
-  {
-    title: "Prova 8",
-    description:
-      "Just adding more elements, so my test is more meaningful. ciao ciao washington>",
-    video:
-      "https://www.youtube.com/watch?v=IF6k0uZuypA&list=PL0vfts4VzfNgUUEtEjxDVfh4iocVR3qIb&index=5",
-    stack: ["Node", "JS", "P5"],
-    source: "https://github.com/Nantakud/my_portfolio/commits/main",
-    visit: "https://google.com",
-    id: 10,
   },
 ];
