361618d20ace43447805448307b5bca5e210e6d4
Nantakud <francesco.vacca.au@gmail.com>
2021-12-27 23:47:17 +0800

    implemented CSS animation

--- src/components/splash-page/Splash.jsx
+++ src/components/splash-page/Splash.jsx
@@ -9,8 +9,19 @@ let sentences = [
   "Phenomenal cosmic powers ... Itty bitty living space.",
 ];
 
+let technologies = [
+  "assets/angular.svg",
+  "assets/ionic.svg",
+  "assets/javascript.svg",
+  "assets/nodejs.svg",
+  "assets/php.svg",
+  "assets/react.svg",
+  "assets/typescript.svg",
+];
+
 export default function Splash() {
   const [timer, setTimer] = useState(0);
+  const [tech, setTech] = useState(0);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -21,10 +32,22 @@ export default function Splash() {
     }, 2500);
   }, [timer]);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      tech > technologies.length - 2 ? setTech(0) : setTech(tech + 1);
+      return function cleanup() {
+        clearTimeout(timeout);
+      };
+    }, 2500);
+  }, [tech]);
+
   return (
     <div className="container">
       <div className="left">
         <img src="assets/genie.png" alt="ilgienio" />
+        <div className="magic">
+          <img src={technologies[tech]} alt="non funzina" />
+        </div>
       </div>
 
       <div className="right">
