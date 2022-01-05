cf36a8dbb6ea8c98f6b7b7bcc565dcf8ffcdce97
Nantakud <francesco.vacca.au@gmail.com>
2022-01-05 23:28:43 +0800

    created a new component called navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "./navbar.scss";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="navbar-wrapper">
+      <div className="links">
+        <Link to="/" onClick={() => setIsOpen(!isOpen)}>
+          Home
+        </Link>
+        <Link to="/about" onClick={() => setIsOpen(!isOpen)}>
+          About
+        </Link>
+        <Link to="/projects" onClick={() => setIsOpen(!isOpen)}>
+          Projects
+        </Link>
+      </div>
+    </div>
+  );
+}
